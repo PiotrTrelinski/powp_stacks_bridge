762627cc7888c0352b2ccb00628d895a66c14621
Piotr Trelinski <195078@edu.p.lodz.pl>
2017-03-09 15:14:18 +0100

    poprawienie błędów konwencji nazw klas, metod, atrybutów itp.

---
+++ src/main/java/edu/kis/vh/stacks/factory/IsStacksFactory.java
@@ -0,0 +1,18 @@
+package edu.kis.vh.stacks.factory;
+
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
+
+public interface IsStacksFactory {
+
+	public Stack getStandardStack();
+
+	public Stack getFalseStack();
+
+	public StackFIFO getFIFOStack();
+
+	public StackHanoi getHanoiStack();
+
+}
+// źle sformatowane wiersze : 7-17
